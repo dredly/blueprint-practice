76e84e50b2daae8f0bdc7a74749c75eb4e86af4c
dredly <dredly@gmail.com>
2022-03-03 00:16:05 +0000

    Made sure it works with static files

--- aat/assessments/__init__.py
+++ aat/assessments/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
-assessments = Blueprint("assessments", __name__, template_folder="templates")
+assessments = Blueprint(
+    "assessments", __name__, template_folder="templates", static_folder="static"
+)
 
 from . import routes
